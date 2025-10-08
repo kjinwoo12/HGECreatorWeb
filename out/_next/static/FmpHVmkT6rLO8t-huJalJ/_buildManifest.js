6afc1fefa2cd3b689289ed0e93781d075f372464
Kim JinWoo <kjinwoo12@gmail.com>
2025-10-09 04:03:53 +0900

    Deploy

---
+++ out/_next/static/FmpHVmkT6rLO8t-huJalJ/_buildManifest.js
@@ -0,0 +1,14 @@
+self.__BUILD_MANIFEST = {
+  "/_error": [
+    "./static/chunks/dff530372c05d9e0.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
